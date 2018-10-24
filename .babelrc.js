1820092e79de34050e561a1bc8b21d394000c10d
akameco <akameco.t@gmail.com>
2018-10-25 00:26:09 +0900

    build(babel): update babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-typescript'],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true }]
-  ],
+  presets: ['zero'],
   ignore: ['**/*.test.ts']
 }
